14ffa2e1d231dc068ba4e718147ff441746174e6
minhphi1012 <phidnm3620@ut.edu.vn>
2025-08-27 22:00:36 +0700

    Create Config.java

---
+++ Config.java
@@ -0,0 +1,9 @@
+public class Config {
+    public static int WIDTH = 800;
+    public static int HEIGHT = 600;
+    public static int PADDLE_WIDTH = 10;
+    public static int PADDLE_HEIGHT = 100;
+    public static int BALL_SIZE = 20;
+    public static int BASE_SPEED = 5;
+    public static int LEVEL_UP_INTERVAL = 3; // nâng độ khó mỗi 3 vòng
+}
